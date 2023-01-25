fcd3f6d1eaed2b552c263189edb39c942070974a
Nikolay Smolov <n.smolov@yandex.ru>
2023-01-25 20:34:23 +0300

    adds draft animation for modal

--- src/ui/modal/index.tsx
+++ src/ui/modal/index.tsx
@@ -1,8 +1,11 @@
-import React, { PropsWithChildren, useEffect } from 'react';
+import React, { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import cn from 'classnames';
-import styles from './styles.module.css';
 import { CloseIcon } from '../icons';
+import { CSSTransition } from 'react-transition-group';
+import { useLocation } from 'react-router-dom';
+import styles from './styles.module.css';
+import './animation-styles.css';
 
 interface IModalProps extends PropsWithChildren {
   title: string;
@@ -12,6 +15,18 @@ interface IModalProps extends PropsWithChildren {
 const modalRoot = document.getElementById('modalRoot') as HTMLElement;
 
 export const Modal: React.FC<IModalProps> = ({ title, children, close }) => {
+  const location = useLocation();
+  const modalRef = useRef(null);
+  const [mount, setMount] = useState(false);
+
+  const handleCloseModal = () => {
+    setMount(false);
+  };
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
   useEffect(() => {
     const handleCloseOnEsc = (evt: KeyboardEvent) => {
       if (evt.key === 'Escape') {
@@ -23,16 +38,30 @@ export const Modal: React.FC<IModalProps> = ({ title, children, close }) => {
     return () => {
       document.removeEventListener('keyup', handleCloseOnEsc);
     };
-  });
+  }, []);
 
   return createPortal(
-    <div className={styles.modal}>
-      <div className={styles.modal__heading}>
-        <p className={cn('text', 'text_type_main-large', 'm-0', styles.modal__title)}>{title}</p>
-        <CloseIcon className={styles.modal__closeIcon} type="interface-secondary" onClick={close} />
+    <CSSTransition
+      nodeRef={modalRef}
+      in={mount}
+      classNames="modal"
+      timeout={150}
+      mountOnEnter
+      unmountOnExit
+      onExited={() => close()}
+    >
+      <div className={styles.modal} ref={modalRef}>
+        <div className={styles.modal__heading}>
+          <p className={cn('text', 'text_type_main-large', 'm-0', styles.modal__title)}>{title}</p>
+          <CloseIcon
+            className={styles.modal__closeIcon}
+            type="interface-secondary"
+            onClick={handleCloseModal}
+          />
+        </div>
+        {children}
       </div>
-      {children}
-    </div>,
+    </CSSTransition>,
     modalRoot
   );
 };
