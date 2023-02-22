e99c09774ad63f2d74e1c0374a5f3e5c09f9424c
Nikolay Smolov <n.smolov@yandex.ru>
2023-02-22 13:20:19 +0300

    adds one more fix prettier complains

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 export function withRouter(
-  WrappedComponent: React.ComponentType,
+  WrappedComponent: React.ComponentType
 ): React.FunctionComponent {
   return function wrappedComponenet() {
     return (
