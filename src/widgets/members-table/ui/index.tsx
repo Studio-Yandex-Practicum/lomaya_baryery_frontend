4fd521f2e763952680bb52367d5c36890518dea7
Алексей <satukov90@yandex.ru>
2023-05-22 21:47:21 +0300

    поправил баг выхода таблицы за пределы блока + добавил Paginator

--- src/widgets/members-table/ui/index.tsx
+++ src/widgets/members-table/ui/index.tsx
@@ -2,8 +2,8 @@ import { useStore } from 'effector-react';
 import { membersModel } from 'entities/members';
 import { MemberRow } from 'entities/members';
 import { Table } from 'shared/ui-kit/table';
-import styles from './styles.module.css';
 import { useMemo } from 'react';
+import styles from './styles.module.css';
 
 interface MembersTableProps {
   extClassName?: string;
@@ -18,10 +18,9 @@ export function MembersTable({ extClassName, counterMembers }: MembersTableProps
   const members = useStore(membersModel.store.$members);
   const filteredMembers = useStore(membersModel.store.$membersStore);
   const header = ['Имя и Фамилия', 'Город', 'Телефон', 'Пройденных смен'];
-  const displayedMembers = useMemo(() => {
-    return filteredMembers &&
-      filteredMembers.slice(counterMembers.firstTableMember, counterMembers.lastTableMember)
-  }, [filteredMembers, counterMembers]);
+  const displayedMembers = useMemo(() => filteredMembers &&
+    filteredMembers.slice(counterMembers.firstTableMember, counterMembers.lastTableMember)
+    , [filteredMembers, counterMembers]);
 
   return (
     <Table
