3aa7ed81c8d50c093c37d44718edbc3f660d82c8
Marat Ikhsanov <ihsmen@gmail.com>
2023-10-09 07:47:59 +0300

    feat: Реализовать возможность просматривать отчеты на мобильном телефоне

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,19 +1,45 @@
 import cn from 'classnames';
 import { useStore } from 'effector-react';
-import { useEffect, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'widgets/header';
 import { SideBar } from 'widgets/sidebar';
 import { shiftModel } from 'entities/shift';
+import { ChevronLeftIcon } from 'shared/ui-kit/icons';
 import { mountLayout } from './model';
 import styles from './styles.module.css';
 
 export const Layout = () => {
   const { isSuccess, isError } = useStore(shiftModel.$shiftsLoading);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  const handleResize = useCallback(() => {
+    setTimeout(() => {
+      setWindowSize(window.innerWidth);
+    }, 500);
+  }, []);
 
   useEffect(() => {
     mountLayout();
   }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
+  useEffect(() => {
+    if (windowSize >= 450) {
+      setIsSideBarOpen(false);
+    }
+  }, [windowSize]);
+
+  const handleOpenSideBar = () => {
+    setIsSideBarOpen((prevValue) => !prevValue);
+  };
 
   const content = useMemo(() => {
     if (isError) {
@@ -26,16 +52,24 @@ export const Layout = () => {
     if (isSuccess) {
       return (
         <>
-          <nav className={styles.navigation}>
+          <nav
+            className={cn({
+              [styles.navigation]: !isSideBarOpen,
+              [styles.navigation_open]: isSideBarOpen,
+            })}
+          >
             <SideBar />
           </nav>
+          <div className={styles.aside} onClick={handleOpenSideBar}>
+            <ChevronLeftIcon color="green" />
+          </div>
           <section className={cn(styles.content, 'custom-scroll')}>
             <Outlet />
           </section>
         </>
       );
     }
-  }, [isSuccess, isError]);
+  }, [isSuccess, isError, isSideBarOpen]);
 
   return (
     <>
